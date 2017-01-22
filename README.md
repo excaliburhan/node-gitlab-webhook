dc90b07f24e0fa784822bcba5277d2d01b4568e6
xiaoping <edwardhjp@gmail.com>
2017-01-22 11:48:55 +0800

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# node-gitlab-webhook
+# node-github-webhook
 Gitlab Webhooks handler based on Node.js. Support multiple handlers.
+
+### Install
+
+`npm install node-gitlab-webhook --save`
+
+### Instructions
+
+This library is modified for Gitlab, Github version here: [node-github-webhook](https://github.com/excaliburhan/node-github-webhook).
+
+If you want to know the settings of Gitlab webhooks, please see: [webhook](https://docs.gitlab.com/ee/web_hooks/web_hooks.html).
+
+### Usage
+
+```js
+var http = require('http')
+var createHandler = require('node-gitlab-webhook')
+var handler = createHandler([ // multiple handlers
+  { path: '/webhook1', secret: 'secret1' },
+  { path: '/webhook2', secret: 'secret2' }
+])
+// var handler = createHandler({ path: '/webhook1', secret: 'secret1' }) // single handler
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(7777)
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log(
+    'Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref
+  )
+  switch(event.path) {
+    case '/webhook1':
+      // do sth about webhook1
+      break
+    case '/webhook2':
+      // do sth about webhook2
+      break
+    default:
+      // do sth else or nothing
+      break
+  }
+    
+})
+```
---
+++ index.js
@@ -0,0 +1,104 @@
+/**
+ * Gitlab Webhooks handler
+*/
+
+var EventEmitter = require('events').EventEmitter
+var inherits = require('util').inherits
+var crypto = require('crypto')
+var bl = require('bl')
+
+function isObject(obj) {
+  return Object.prototype.toString.apply(obj) === '[object Object]'
+}
+
+function findHandler(url, arr) {
+  var ret = arr[0]
+  for (var i = 0; i < arr.length; i++) {
+    if (url.split('?').shift() === arr[i].path)
+      ret = arr[i]
+  }
+  return ret
+}
+
+function create(options) {
+  // make it an EventEmitter, sort of
+  handler.__proto__ = EventEmitter.prototype
+  EventEmitter.call(handler)
+
+  return handler
+
+  function handler(req, res, callback) {
+    function hasError(msg) {
+      res.writeHead(400, { 'content-type': 'application/json' })
+      res.end(JSON.stringify({ error: msg }))
+
+      var err = new Error(msg)
+      handler.emit('error', err, req)
+      callback(err)
+    }
+
+    function checkType(options) {
+      if (!isObject(options))
+          throw new TypeError('must provide an options object')
+
+      if (typeof options.path !== 'string')
+        throw new TypeError('must provide a \'path\' option')
+      
+      if (typeof options.secret !== 'string')
+        throw new TypeError('must provide a \'secret\' option')
+    }
+
+    var currentOptions
+    if (Array.isArray(options)) {
+      currentOptions = findHandler(req.url, options)
+    } else {
+      currentOptions = options
+    }
+
+    checkType(currentOptions)
+
+    if (req.url.split('?').shift() !== currentOptions.path)
+      return callback()
+    
+    var event = req.headers['x-gitlab-event']
+    var events = currentOptions.events
+
+    if (!event)
+      return hasError('No X-Gitlab-Event found on request')
+    
+    if (events && events.indexOf(event) == -1)
+      return hasError('X-Gitlab-Event is not acceptable')
+    
+    req.pipe(bl(function(err, data) {
+      if (err)
+        return hasError(err.message)
+
+      var obj
+
+      try {
+        obj = JSON.parse(data.toString())
+      } catch (e) {
+        return hasError(e)
+      }
+
+      var event = obj.object_kind
+
+     res.writeHead(200, { 'content-type': 'application/json' })
+     res.end('{"ok":true}')
+
+      var emitData = {
+        event: event,
+        payload: obj,
+        protocol: req.protocol,
+        host: req.headers['host'],
+        url: req.url,
+        path: currentOptions.path
+      }
+
+      handler.emit(event, emitData)
+      handler.emit('*', emitData)
+    }))
+  }
+}
+
+module.exports = create
